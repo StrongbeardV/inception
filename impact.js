ce8c07124b2f1b2ad1eea853ce8d4b06017d5895
StrongbeardV <87314942+StrongbeardV@users.noreply.github.com>
2022-11-30 15:56:28 +0100

    Update impact.js

--- impact.js
+++ impact.js
@@ -1,21 +1,21 @@
 /*LINE: CLIMATE OFFSET NEW*/
 const labelarray = [];
-const datapointarray_1SC_carbon = [];
-const datapointarray_1SC_carbon_cum = [];
+const datapointarray_1sc_carbon = [];
+const datapointarray_1sc_carbon_cum = [];
 
 $('.datalabelitem_carbon_month').each(function() {
     const labelitem = $(this).text();
     labelarray.push(labelitem);
 });
 
-$('.datapoint_1SC_carbon-offset').each(function() {
+$('.datapoint_1sc_carbon-offset').each(function() {
     const datapointitem = $(this).text();
-    datapointarray_1SC_carbon.push(datapointitem);
+    datapointarray_1sc_carbon.push(datapointitem);
 });
 
-$('.datapoint_1SC_carbon-offset-cum').each(function() {
+$('.datapoint_1sc_carbon-offset-cum').each(function() {
     const datapointitem = $(this).text();
-    datapointarray_1SC_carbon_cum.push(datapointitem);
+    datapointarray_1sc_carbon_cum.push(datapointitem);
 });
 
 const ctx_carbon_new = document.getElementById('carbon-offset-new').getContext('2d');
@@ -26,14 +26,14 @@ const mixedChart = new Chart(ctx_carbon_new, {
         		type: 'line',
             label: 'Carbon Offset cumulative',
             //borderDash: [5, 5],
-            data: datapointarray_1SC_carbon_cum,
+            data: datapointarray_1sc_carbon_cum,
             backgroundColor: 'white',
             borderColor: '#49654C',
             borderWidth: 3
         }, {
         		type: 'bar',
             label: 'Carbon Offset monthly',
-            data: datapointarray_1SC_carbon,
+            data: datapointarray_1sc_carbon,
             backgroundColor: [
                 'rgb(164,117,124)',
             ],
